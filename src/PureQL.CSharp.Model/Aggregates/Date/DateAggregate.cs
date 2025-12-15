3a418f7abcc533ad1e1dd3b9d7075dd483535d86
Dmitry Kurochkin <kudima03@gmail.com>
2025-12-15 14:49:58 +0300

    Implemented DateAggregate

---
+++ src/PureQL.CSharp.Model/Aggregates/Date/DateAggregate.cs
@@ -0,0 +1,19 @@
+using OneOf;
+
+namespace PureQL.CSharp.Model.Aggregates.Date;
+
+public sealed class DateAggregate
+    : OneOfBase<MaxDate, MinDate, AverageDate>
+{
+    public DateAggregate(AverageDate averageDate)
+        : this((OneOf<MaxDate, MinDate, AverageDate>)averageDate) { }
+
+    public DateAggregate(MinDate minDate)
+        : this((OneOf<MaxDate, MinDate, AverageDate>)minDate) { }
+
+    public DateAggregate(MaxDate maxDate)
+        : this((OneOf<MaxDate, MinDate, AverageDate>)maxDate) { }
+
+    private DateAggregate(OneOf<MaxDate, MinDate, AverageDate> input)
+        : base(input) { }
+}
