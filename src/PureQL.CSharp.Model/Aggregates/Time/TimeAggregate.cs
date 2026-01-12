1d71f54972612ebcde8b5afd8c3ea4ec8fe5f8db
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-12 14:52:11 +0300

    Fixed naming

--- src/PureQL.CSharp.Model/Aggregates/Time/TimeAggregate.cs
+++ src/PureQL.CSharp.Model/Aggregates/Time/TimeAggregate.cs
@@ -2,17 +2,17 @@ using OneOf;
 
 namespace PureQL.CSharp.Model.Aggregates.Time;
 
-public sealed class TimeAggregate : OneOfBase<MaxTime, MinTime, AveradeTime>
+public sealed class TimeAggregate : OneOfBase<MaxTime, MinTime, AverageTime>
 {
-    public TimeAggregate(AveradeTime averageTime)
-        : this((OneOf<MaxTime, MinTime, AveradeTime>)averageTime) { }
+    public TimeAggregate(AverageTime averageTime)
+        : this((OneOf<MaxTime, MinTime, AverageTime>)averageTime) { }
 
     public TimeAggregate(MinTime minTime)
-        : this((OneOf<MaxTime, MinTime, AveradeTime>)minTime) { }
+        : this((OneOf<MaxTime, MinTime, AverageTime>)minTime) { }
 
     public TimeAggregate(MaxTime maxTime)
-        : this((OneOf<MaxTime, MinTime, AveradeTime>)maxTime) { }
+        : this((OneOf<MaxTime, MinTime, AverageTime>)maxTime) { }
 
-    private TimeAggregate(OneOf<MaxTime, MinTime, AveradeTime> input)
+    private TimeAggregate(OneOf<MaxTime, MinTime, AverageTime> input)
         : base(input) { }
 }
