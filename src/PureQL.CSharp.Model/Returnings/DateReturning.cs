945433cc604278ecd2e65fb8bb19f9ac86a6deaa
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-26 15:32:41 +0300

    Removed DateField

--- src/PureQL.CSharp.Model/Returnings/DateReturning.cs
+++ src/PureQL.CSharp.Model/Returnings/DateReturning.cs
@@ -1,21 +1,17 @@
 using OneOf;
-using PureQL.CSharp.Model.Fields;
 using PureQL.CSharp.Model.Parameters;
 using PureQL.CSharp.Model.Scalars;
 
 namespace PureQL.CSharp.Model.Returnings;
 
-public sealed class DateReturning : OneOfBase<DateField, DateParameter, DateScalar>
+public sealed class DateReturning : OneOfBase<DateParameter, DateScalar>
 {
-    public DateReturning(DateField field)
-        : this((OneOf<DateField, DateParameter, DateScalar>)field) { }
-
     public DateReturning(DateParameter parameter)
-        : this((OneOf<DateField, DateParameter, DateScalar>)parameter) { }
+        : this((OneOf<DateParameter, DateScalar>)parameter) { }
 
     public DateReturning(DateScalar scalar)
-        : this((OneOf<DateField, DateParameter, DateScalar>)scalar) { }
+        : this((OneOf<DateParameter, DateScalar>)scalar) { }
 
-    private DateReturning(OneOf<DateField, DateParameter, DateScalar> input)
+    private DateReturning(OneOf<DateParameter, DateScalar> input)
         : base(input) { }
 }
