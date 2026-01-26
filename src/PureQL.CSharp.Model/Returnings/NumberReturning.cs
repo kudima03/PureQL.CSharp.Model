37b9956bab7d561c047d6b503a350d68047eb538
Dmitry Kurochkin <kudima03@gmail.com>
2026-01-26 15:33:05 +0300

    Removed NumberField

--- src/PureQL.CSharp.Model/Returnings/NumberReturning.cs
+++ src/PureQL.CSharp.Model/Returnings/NumberReturning.cs
@@ -1,22 +1,17 @@
 using OneOf;
-using PureQL.CSharp.Model.Fields;
 using PureQL.CSharp.Model.Parameters;
 using PureQL.CSharp.Model.Scalars;
 
 namespace PureQL.CSharp.Model.Returnings;
 
-public sealed class NumberReturning
-    : OneOfBase<NumberField, NumberParameter, NumberScalar>
+public sealed class NumberReturning : OneOfBase<NumberParameter, NumberScalar>
 {
-    public NumberReturning(NumberField field)
-        : this((OneOf<NumberField, NumberParameter, NumberScalar>)field) { }
-
     public NumberReturning(NumberParameter parameter)
-        : this((OneOf<NumberField, NumberParameter, NumberScalar>)parameter) { }
+        : this((OneOf<NumberParameter, NumberScalar>)parameter) { }
 
     public NumberReturning(NumberScalar scalar)
-        : this((OneOf<NumberField, NumberParameter, NumberScalar>)scalar) { }
+        : this((OneOf<NumberParameter, NumberScalar>)scalar) { }
 
-    private NumberReturning(OneOf<NumberField, NumberParameter, NumberScalar> input)
+    private NumberReturning(OneOf<NumberParameter, NumberScalar> input)
         : base(input) { }
 }
